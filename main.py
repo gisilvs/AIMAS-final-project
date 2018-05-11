c73ce56b18476c49ff249da826dd724bd0031c0b
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2018-05-11 16:28:12 +0200

    working on shades

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from shapely import geometry
 
 class Repeater():
 
-    def __init__(self, pos):
+    def __init__(self, pos,index):
         self.position = pos
         self.velocity = np.array((0, 0), dtype=float)
 
         self.error_prev = np.array((0, 0))
+        self.index=index
 
     def control(self, pos_desired, repeaters = [], obstacles = []):
         """
@@ -21,7 +22,7 @@ class Repeater():
         """
 
 
-        kr = 2 # Repulsive gain
+        kr = 50 # Repulsive gain
         S = 10 # completely randomly set. todo: pick a good value.
         R = 2 # radius of repeater (obstacle)
 
@@ -33,14 +34,15 @@ class Repeater():
                 d = norm(repeater.position - self.position)
 
                 # If repeater is far away, don't account for it at all
-                if d > S or d == 0: # todo: do something smarter to avoid checking himself?
+                # or if we have priority
+                if d > S or d == 0 or self.index<repeater.index: # todo: do something smarter to avoid checking himself?
                     continue
 
                 # Direction from repeater to self, normalized
                 direction = (repeater.position - self.position) / d
 
                 # If very close, large repulsive force
-                if d >= R:
+                if d <= R:
                     repulsive += direction * 1000 # todo: not a good idea
 
                 else:
@@ -191,7 +193,7 @@ for point in bounding_polygon:
 
 sh_bounding_polygon=geometry.Polygon(bounding_polygon)
 min_x,min_y,max_x,max_y=sh_bounding_polygon.bounds
-not_seen=discretize(min_x,max_x,min_y,max_y,30)
+not_seen=discretize(min_x,max_x,min_y,max_y,10)
 
 repeaters=[]
 
@@ -225,7 +227,7 @@ while not done:
         # if we have repeaters we move them
         if norm(repeaters[-1].position-ground_station)>=desired_range:
             #if the last added repeater is going out of rage from the ground station we add a new one
-            repeaters.append(Repeater(ground_station.copy()))
+            repeaters.append(Repeater(ground_station.copy(),len(repeaters)))
         for r in range(len(repeaters)):
             #for all the repeaters, if is the one following the payload we move towards it,
             # otherwise we move towards the next repeater in the chain
@@ -239,7 +241,7 @@ while not done:
     #if there are no repeaters, we check if the payload is on range.
     # if not, we add a repeater
         if norm(traj_pos[time_step]-ground_station)>=desired_range:
-            repeaters.append(Repeater(ground_station.copy()))
+            repeaters.append(Repeater(ground_station.copy(),0))
 
 
 
