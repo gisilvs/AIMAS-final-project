a2cf9f89805a8e4a0cc0cb498edf0791b4ac5a81
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2018-05-12 13:17:38 +0200

    need to change bounding polygon

--- main.py
+++ main.py
@@ -172,20 +172,20 @@ def update_map(position, squares, sh_bounding_polygon, obstacle_matrix):
     for i  in range(squares.shape[0]):
         for j in range (squares.shape[1]):
             square=squares[i,j]
-            if square:
+            if not square['seen']:
                 center = square['center']
                 if norm(position-center) < scanning_range:
                     seen = True
 
                     # Check if it intersects with obstacle / boundary
-                    line = geometry.LineString([geometry.Point(center), geometry.Point(position)])
-                    if line.intersects(sh_bounding_polygon):
+                    line = geometry.LineString([center, position])
+                    if line.intersects(sh_bounding_polygon.exterior):
                         obstacle_matrix[i, j] = 1
 
                 else:
                     seen=False
                 if seen:
-                    squares[square['i'],square['j']]=None
+                    squares[square['i'],square['j']]['seen']=True
 
     return squares, obstacle_matrix
 
@@ -196,10 +196,14 @@ def find_boundary(squares):
     :param squares: Array with elements that are squares if they are not seen, otherwise None
     :return: A set with center coordinates for the boundary squares
     """
+    indices=[]
+    for i,row in enumerate(squares):
+        for j,square in enumerate (row):
+            if square['seen']:
+               indices.append((i,j))
 
-    indices=np.argwhere(squares==None)
     boundary=set()
-    for index in  indices:
+    for index in indices:
         i,j=index
         xs=[]
         ys=[]
@@ -212,12 +216,12 @@ def find_boundary(squares):
         if j < squares.shape[1]-1:
             ys.append(j+1)
         for x in xs:
-            if squares[x,j]!=None:
+            if not squares[x,j]['seen']:
                 boundary.add((squares[x,j]['i'],squares[x,j]['j']))
             for y in ys:
-                if squares[i, y] != None:
+                if not squares[i, y]['seen']:
                     boundary.add((squares[i,y]['i'],squares[i,y]['j']))
-                if squares[x,y] != None:
+                if not squares[x,y]['seen']:
                     boundary.add((squares[x,y]['i'],squares[x,y]['j']))
 
     return boundary
@@ -256,7 +260,7 @@ def discretize(bounds, n_squares):
         for j in range(len(ys)-1):
             vertices = [(xs[i],ys[j]),(xs[i],ys[j+1]),(xs[i+1],ys[j+1]),(xs[i+1],ys[j])]
             square=geometry.Polygon(vertices)
-            squares[i,j]={'square':vertices, 'center':np.array(square.centroid), 'i':i,'j':j}
+            squares[i,j]={'square':vertices, 'center':np.array(square.centroid), 'i':i,'j':j,'seen':False}
     return squares
 
 def list_to_pygame(list_of_points):
@@ -274,7 +278,7 @@ def to_pygame(coords):
     return (int(coords[0] * 5 + width / 2 - 150), int(coords[1] * -5 + height / 2 + 200))
 
 
-def set_bg(repeaters,squares):
+def set_bg(repeaters,squares,obstacle_matrix):
 
     """
     set initial and final position
@@ -310,8 +314,10 @@ def set_bg(repeaters,squares):
     for i in range(squares.shape[0]):
         for j in range(squares.shape[1]):
             square = squares[i, j]
-            if square:
+            if not square['seen']:
                 pg.draw.polygon(s,(100,100,100,128), list_to_pygame(square['square']))
+            if obstacle_matrix[i,j]==1:
+                pg.draw.polygon(screen, (222, 184, 135), list_to_pygame(square['square']))
 
     screen.blit(s, (0, 0))
 # PyGame parameters
@@ -341,7 +347,7 @@ traj_theta=traj["theta"]
 traj_x=traj["x"]
 traj_y=traj["y"]
 traj_pos=np.array(list(zip(traj_x,traj_y)))
-traj_pos-=traj_pos[0]
+traj_pos-=traj_pos[0]+np.array((0.1,0.1))
 
 dt=0.1
 
@@ -350,7 +356,9 @@ for point in bounding_polygon:
     pg_bounding_polygon.append(to_pygame(point))
 
 sh_bounding_polygon=geometry.Polygon(bounding_polygon)
+bounding_lines=[()]
 bounds = sh_bounding_polygon.bounds
+sh_bounding_lines=geometry.LineString()
 n_squares = 30
 squares=discretize(bounds, n_squares)
 obstacle_matrix = np.zeros(squares.shape)
@@ -411,5 +419,5 @@ while not done:
     time_step += 1
 
     if time_step%5==0:
-        set_bg(repeaters,squares)
+        set_bg(repeaters,squares,obstacle_matrix)
         pg.display.flip()
