0defa77fef3b9d474c69d17fd04b50cf74a880a3
Alex Hermansson <grobgeld@kth.se>
2018-05-21 11:58:51 +0200

    cleaned a bit

--- conf.py
+++ conf.py
@@ -2,10 +2,10 @@
 kp = 10
 kd = 2
 
-#Force field multiplication constants
-G_r = 5 #repulsion from other repeaters and main drone
-G_s = 5 #repulsion from obstacles and unseen areas
-G_n = 0.5 #noise repulsion
+# Force field multiplication constants
+G_r = 5 # repulsion from other repeaters and main drone
+G_s = 5 # repulsion from obstacles and unseen areas
+G_n = 0.5 # noise repulsion
 
 # LOS parameters
 wm = 2 # for distance to closest obstacle/unseen area
@@ -16,7 +16,7 @@ n_samples = 10 # number of points to evaluate. If bigger, better result but slow
 n_squares = 40 # n*n squares in the discretised space. If bigger, better simulation but slower
 plot_step = 4 # update plot after n steps (bigger to speed up).
 
-dt=0.1
+dt = 0.1
 
-v_range=2 # used to find the next position for a repeater.
-use_sqrt=False #use square root in the optimization function or not
+v_range = 2 # used to find the next position for a repeater.
+use_sqrt = False # use square root in the optimization function or not
